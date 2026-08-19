7996b9158af5ab2d0bd07759819cb2fd146e7cc3
Umer Tanveer <umer@oilsense.ai>
2026-08-19 22:37:30 +0500

    chore(git): ignore papers directory and academic manuscript artifacts

--- papers/paper_edge_effects/compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Compiling Paper 3: Spatial Edge Effects...
-pdflatex -interaction=nonstopmode main.tex
-bibtex main
-pdflatex -interaction=nonstopmode main.tex
-pdflatex -interaction=nonstopmode main.tex
-echo.
-echo Done! Check main.pdf for output.
